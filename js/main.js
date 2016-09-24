9436d576fbfa214185cb24b41445b8765a46641a
Sashik Zatynin <tochcha@gmail.com>
2016-09-24 16:21:04 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+document.createElement('header');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('footer');
+document.createElement('figure');
+document.createElement('figcaption');
+document.createElement('main');
+	
+	
+});
